6af182d5e6ef7def64bc1187cd3de4bf8dafd7c6
Adrian De León <adriandleon@gmail.com>
2025-08-30 22:09:04 -0300

    Add pro-guard rules

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -52,8 +52,36 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-    buildTypes { getByName("release") { isMinifyEnabled = false } }
+    packaging.resources {
+        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        excludes += "/META-INF/licenses/ASM"
+        excludes += "/META-INF/LICENSE.md"
+        excludes += "/META-INF/LICENSE-notice.md"
+        pickFirsts += "win32-x86-64/attach_hotspot_windows.dll"
+        pickFirsts += "win32-x86/attach_hotspot_windows.dll"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isShrinkResources = false
+            isMinifyEnabled = false
+            isDebuggable = true
+            testProguardFiles("proguard-test-rules.pro")
+        }
+
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            isDebuggable = false
+            ndk.debugSymbolLevel = "FULL"
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
